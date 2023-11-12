e819c64ba15f7b27aaad2f414407b7e421564e2a
mrdog233o5 <mr.dog233o5@gmail.com>
2023-11-12 15:25:05 +0800

    added butttons

--- jogpm/jogpm/ContentView.swift
+++ jogpm/jogpm/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("jogpmMode") var jogpmMode: JogpmMode = .gen
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            Text("JogPM")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding([.horizontal], 50.0)
+                .padding([.vertical], 5.0)
+            VStack {
+                ForEach(JogpmMode.allCases, id: \.self) { item in
+                    Button {
+                        
+                    } label: {
+                        Text(item.rawValue)
+                            .foregroundColor(Color.primary)
+                            .frame(width: 100.0)
+                    }
+                }
+            } .frame(height: 80)
+        }
     }
 }
 
